231f7147fd72c9b6c7dec1aef97c1284964206e6
Anthony Wiggins <thewiggins21@gmail.com>
2024-04-07 00:10:54 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Pro_README_Generator
+
+## Description
+
+Created a command-line application that dynamically generates a professional README.md file from a user's input using the Inquirer package. This allows the project creator to devote more time to working on the project.
+
+## Screenshot of Site
+
+![screenshot]()
+
+## Links
+
+[GitHub]()
+
+[Repo]()
+
+[Demo]()
---
+++ index.js
@@ -0,0 +1,66 @@
+// packages needed for this application
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+// array of questions for user input
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'Please input your project title?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Please input a description of your project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Please input installation instructions:',
+      default: 'npm i',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Please input usage information:',
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Please input contribution guidelines:',
+    },
+    {
+      type: 'input',
+      name: 'test',
+      message: 'Please input test instructions:',
+      default: 'npm test',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Please select license for application from the list of options:',
+      choices: ['None','MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3'],
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Please input your GitHub username:',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Please input your email address:',
+    },
+];
+
+// function to write README file
+function writeToFile(fileName, data) {}
+
+// function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
